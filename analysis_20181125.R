d325ec30a62e9ce9ea887419cec13ea8a5ce0025
trainorp <patrick.trainor@louisville.edu>
2018-11-29 09:31:28 -0800

    working

--- analysis_20181125.R
+++ analysis_20181125.R
@@ -4,8 +4,18 @@ library(pillar)
 library(tidyverse)
 
 setwd("~/gdrive/AthroMetab/WCMC")
+
+# Import WCMC data:
 df1<-readxl::read_xlsx("targetedData_20181123.xlsx")
 metabKey<-readxl::read_xlsx("metabKey_20181123.xlsx")
+
+# Import cohort phenotypes:
+oxPLDF<-read.csv("~/gdrive/Athro/oxPL6/wide_data_20150529.csv")
+phenoDF<-oxPLDF %>% select(ptid,group=migroup2,coarseGroup=group)
+phenoDF$group[is.na(phenoDF$group)]<-"sCAD"
+phenoDF$group[phenoDF$group=="Type 1"]<-"Thrombotic MI"
+phenoDF$group[phenoDF$group=="Type 2"]<-"Non-Thrombotic MI"
+phenoDF$ptid<-as.character(phenoDF$ptid)
 
 ############ Data processing ############
 # QC data:
@@ -22,7 +32,8 @@ df1$samp<-gsub("MtdBlank_postDeFilippis0","Blank",df1$samp)
 df1$ptid<-gsub("Blank[[:digit:]]","",gsub("QC[[:digit:]]","",
                                           str_split(df1$samp,"-",simplify=TRUE)[,1]))
 df1$timept<-str_split(df1$samp,"-",simplify=TRUE)[,2]
-phenoDF<-df1 %>% select(samp,ptid,timept)
+phenoDF<-df1 %>% select(samp,ptid,timept) %>% left_join(phenoDF)
+df1<-df1 %>% select(-samp)
 
 ############ QC samples throughout run ############
 qcMeans<-qcDFL %>% group_by(metabID) %>% summarize(meanConc=mean(Concentration))
@@ -41,3 +52,6 @@ png(file="qcPlot1.png",height=4,width=7,units="in",res=300)
 ggplot(qcPlotDF,aes(x=samp,y=log(Concentration),group=Metabolite,color=Metabolite)) + 
   geom_point() + geom_line() + theme_bw() + xlab("Sample")
 dev.off()
+
+############ Da ############
+
